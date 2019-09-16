118cd789b3d208eb2c6ec664cfb62565eccd7d75
Prabakaran.A <prabakaran1993@gmail.com>
2019-09-17 01:34:30 +0530

    Update docker-installation-steps.sh

--- docker-installation-steps.sh
+++ docker-installation-steps.sh
@@ -1,3 +1,10 @@
-sudo yum install apt-transport-https     ca-certificates     curl     gnupg-agent     software-properties-common
+1. CentOS Minimal
 
+2. newtork
+nmcli
+nmtui
 
+3. ssh
+nano /etc/ssh/sshd_config
+set GSSAPIAuthentication no
+UseDNS no
